fa1afcfe18ea48a0229d6aa2e4944f6937461d6e
John O <JohnOBrien@Johns-MacBook-Pro.local>
2018-09-18 09:11:33 -0400

    Created problem2 file and method
    evenFibonacciSum method adds all even fibonacci numbers below upperbound

--- problem2.java
+++ problem2.java
@@ -2,4 +2,20 @@ public class problem2{
 	public static void main(String[] args){
 
 	}
+
+	public static int evenFibonacciSum(int upperBound,int firstNum, int secondNum){
+		int lastNum=firstNum;
+		int currNum=secondNum;
+		int sum=0;
+		if(lastNum%2==0){
+			sum+=lastNum;
+		}
+		while(currNum<=upperBound){
+			if(currNum%2==0){
+				sum+=currNum;
+			}
+			currNum+=lastNum;
+		}
+		return sum;
+	}
 }
