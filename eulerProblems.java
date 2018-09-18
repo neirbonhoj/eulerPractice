8b37f205b0e4b6f6e04f52b4155b3a0f79171bc0
John O <JohnOBrien@Johns-MacBook-Pro.local>
2018-09-18 09:05:13 -0400

    Created final test and emoved problem 2

--- eulerProblems.java
+++ eulerProblems.java
@@ -1,12 +1,11 @@
 public class eulerProblems{
 	public static void main(String[] args){
-		System.out.println(multiples35(1000));
+		System.out.println("Problem 1: Sum of multiples of 3 and 5 below 1,000 is "+multiples35(1000)+".");
 	}
 
 	public static int multiples35(int upperBound){
 		int sum=0;
 		for(int i=0;i<upperBound;i++){
-			System.out.println(i);
 			if(i%3==0){
 				sum+=i;
 			} else if(i%5==0){
@@ -14,9 +13,5 @@ public class eulerProblems{
 			}
 		}
 		return sum;
-	}
-
-	public static int fibonacciSum(int upperBound){
-		
 	}
 }
