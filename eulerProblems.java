52c71145f0e999192220aec47afc5bce974af356
John O <JohnOBrien@Johns-MacBook-Pro.local>
2018-09-18 08:59:16 -0400

    Changed loop to not include upperBound

--- eulerProblems.java
+++ eulerProblems.java
@@ -5,7 +5,8 @@ public class eulerProblems{
 
 	public static int multiples35(int upperBound){
 		int sum=0;
-		for(int i=0;i<=upperBound;i++){
+		for(int i=0;i<upperBound;i++){
+			System.out.println(i);
 			if(i%3==0){
 				sum+=i;
 			} else if(i%5==0){
