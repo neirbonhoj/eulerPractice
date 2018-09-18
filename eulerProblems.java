73a93f1e5f256ca0bd91f5159ef749ec3b929222
John O <JohnOBrien@Johns-MacBook-Pro.local>
2018-09-18 08:51:52 -0400

    Initialied Repository

---
+++ eulerProblems.java
@@ -0,0 +1,15 @@
+public class eulerProblems{
+	public static void main(String[] args){
+		System.out.println(multiples35(10));
+	}
+
+	public int multiples35(int upperBound){
+		int sum=0;
+		for(int i=0;i<=upperBound,i++){
+			if(i%3==0 || i%5==0){
+				sum+=i;
+			}
+		}
+		return sum;
+	}
+}
