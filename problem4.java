7486160474dc6e2cd33c0477cf709a4cd387c110
John O <JohnOBrien@Johns-MacBook-Pro.local>
2018-09-20 11:56:21 -0400

    method attempt

--- problem4.java
+++ problem4.java
@@ -8,8 +8,20 @@ public class problem4{
 		for(int i=1;i<digit;i++){
 			size*=10;
 		}
-		int num1=size;
-		int num2=size;
-		System.out.println(num1+","+num2);
+		int num=size;
+		String lPali="";
+		for(int i=num;i<size*10-1;i++){
+			for(int j=num;j<size*10-1;j++){
+				String numS = num+"";
+				String reverse="";
+				for(int k=numS.length()-1;k>=0;k--){
+					reverse+=numS.substring(k,k+1);
+				}
+				if(numS.equals(reverse)){
+					lPali=numS;
+				}
+			}
+		}
+		System.out.println(lPali);
 	}
 }
