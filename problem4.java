9ea63f828e3b1c123a0226b1bceb3d614041c3b0
John O <JohnOBrien@Johns-MacBook-Pro.local>
2018-09-20 10:18:44 -0400

    created problem4 file

--- problem4.java
+++ problem4.java
@@ -1,5 +1,15 @@
 public class problem4{
 	public static void main(String[] args){
-	
+		largestPalindrome(2);
+	}
+
+	public static void largestPalindrome(int digit){
+		int size=1;
+		for(int i=1;i<digit;i++){
+			size*=10;
+		}
+		int num1=size;
+		int num2=size;
+		System.out.println(num1+","+num2);
 	}
 }
