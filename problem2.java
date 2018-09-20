c0f2e1f6140519427c18faa538d7f6d75e632fdd
John O <JohnOBrien@Johns-MacBook-Pro.local>
2018-09-20 09:50:08 -0400

    Updated tester and added commafy

--- problem2.java
+++ problem2.java
@@ -1,6 +1,6 @@
-public class problem2{
+public class problem2 extends commafy{
 	public static void main(String[] args){
-		System.out.println("The sum of all even fibonacci numbers between 1 and 10 is "+evenFibonacciSum(10,1,2));
+		System.out.println("The sum of all even fibonacci numbers between 1 and 10 is "+commafyInt(evenFibonacciSum(4000000,1,2)));
 	}
 
 	public static int evenFibonacciSum(int upperBound,int firstNum, int secondNum){
@@ -8,7 +8,9 @@ public class problem2{
 		int numTwo=secondNum;
 		int sum=0;
 		while(numTwo<=upperBound){
-			sum+=secondNum;
+			if(numTwo%2==0){
+				sum+=numTwo;
+			}
 			int newNum = numOne+numTwo;
 			numOne=numTwo;
 			numTwo=newNum;
