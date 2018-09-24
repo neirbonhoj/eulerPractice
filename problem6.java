0a562ee402207307e499534db72ecc00436c688e
John O <JohnOBrien@Johns-MacBook-Pro.local>
2018-09-24 11:23:36 -0400

    Solved problem6

--- problem6.java
+++ problem6.java
@@ -1,5 +1,15 @@
 public class problem6{
 	public static void main(String[] args){
-	
+		System.out.println(differenceOfSquares(100));
+	}
+	public static int differenceOfSquares(int upper){
+		int sumSquares=0;
+		int squaredSum=0;
+		for(int i=1;i<=upper;i++){
+			sumSquares+=(i*i);
+			squaredSum+=i;
+		}
+		squaredSum*=squaredSum;
+		return (squaredSum-sumSquares);
 	}
 }
